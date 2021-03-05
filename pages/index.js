4439c927beb1074bb15f06480d62cf178129cb0d
krav maguy <flex4lease@gmail.com>
2021-03-05 04:33:14 -0600

    pokiemon name

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,26 @@
 // Every Pokemon card will have the name, picture, their number, and their type
 // For styling, only use styled-jsx that came with nextjs
 
-function Home() {
-  return <h1>Learn next.js: pokemon example</h1>
-
-  // Render posts...
+function Home({ posts }) {
+  console.log(posts.results, "posts");
+  return (
+    <>
+      <h1>Learn next.js: pokemon example</h1>
+      <h2>Pokiemons :</h2>
+      <ul>
+        {posts.results.map((pokiemon) => (
+          <li>{pokiemon.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon')
-  const posts = await res.json()
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+  const posts = await res.json();
 
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
@@ -25,7 +34,7 @@ export async function getStaticProps() {
     props: {
       posts,
     },
-  }
+  };
 }
 
-export default Home
+export default Home;
