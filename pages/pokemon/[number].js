e6de21293274817dfe7286e798043c67c7a09047
krav maguy <flex4lease@gmail.com>
2021-03-05 10:03:17 -0600

    picture

--- pages/pokemon/[number].js
+++ pages/pokemon/[number].js
@@ -1,10 +1,19 @@
 import Link from "next/link";
 
-const pokemon = ({ pokemon }) => {
+const pokemon = ({ pokemon, number }) => {
   console.log("in the pokemon page:", pokemon);
+  const { types } = pokemon;
   return (
     <div>
       <h1>{pokemon.name}</h1>
+      <img
+        src={`https://pokeres.bastionbot.org/images/pokemon/${number}.png`}
+        alt={pokemon.name}
+        width="200"
+        height="200"
+      />
+      <p>number: {number}</p>
+      <p>type : {types[0].type.name}</p>
       <div>
         <Link href="/">
           <a>back</a>
@@ -18,7 +27,7 @@ pokemon.getInitialProps = async ({ query }) => {
   const number = query.number;
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
   const pokemon = await res.json();
-  return { pokemon };
+  return { pokemon, number };
 };
 
 export default pokemon;
