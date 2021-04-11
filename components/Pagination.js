4b26b25306e1504965210da58da93b261e650872
krav maguy <flex4lease@gmail.com>
2021-04-10 23:49:44 -0500

    fixed isNaN button

--- components/Pagination.js
+++ components/Pagination.js
@@ -2,11 +2,11 @@ function Pagination({ prevPage, Prev, Next }) {
   console.log(prevPage,'page')
   return (
     <div className="grid">
-      {prevPage && (
+      {(prevPage &&! isNaN(prevPage) &&prevPage!=="NaN")?(
         <button className="card buttonText" onClick={Prev}>
           Prev
         </button>
-      )}
+      ):null}
       <button className="card buttonText" onClick={Next}>
         Next
       </button>
